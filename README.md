8d6eefc006faef99f0536400f7f0ad804cf6db52
vancillo <yaredbane@gmail.com>
2023-05-27 13:39:42 -0600

    Returns a list of products coming from a JSON

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Ecommerce
 
-- Muestra una lista de productos que vienen de un JSON
+✅ Muestra una lista de productos que vienen de un JSON
 - Añade un filtro por categoría
 - Añade un filtro por precio
 
---
+++ src/components/Products.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+import './Products.css'
+import { AddToCartIcon } from './Icons'
+
+export default function Products ({ products }) {
+  return(
+    <main className='products'>
+        <ul>
+          {products.slice(0, 10).map(product => (
+            <li key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+                <div>
+                    <strong>{product.title}</strong> -${product.price}
+                </div>
+             <div>
+                <button>
+                  <AddToCartIcon />
+                </button>
+            </div>
+            </li>
+            ))}
+        </ul>
+    </main>
+ )
+}
